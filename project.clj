89f54a81cdbd8e4672fa76a4ff927d8d7e370194
sharp.maestro <sharp.maestro@gmail.com>
2016-06-26 00:17:03 +0300

    * fix project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject page-renderer "0.1.0"
   :description "Page renderer for pure Clojure pages (uses Hiccup and Garden)"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/ognivo/page-renderer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
