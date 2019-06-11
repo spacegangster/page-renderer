6edb02fc54deabd161dd5f287978d9fb1e0b9e7b
Ivan Fedorov <hello@spacegangster.io>
2019-06-11 22:57:36 +0300

    project.clj: fix url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject page-renderer "0.2.1"
   :description "A bootstrapped page renderer for pure Clojure pages (uses Hiccup and Garden)"
-  :url "https://github.com/ognivo/page-renderer"
+  :url "https://github.com/spacegangster/page-renderer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
